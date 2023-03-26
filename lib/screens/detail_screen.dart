c6044741cf59d30b7516df770f4d3208aa3dd61c
김태웅 <rlaxodnd95@gmail.com>
2023-03-26 16:26:21 +0900

    Detail 페이지   - 상세 정보 불러오기 및 데이터 노출

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/model/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+import '../model/webtoon_detail_model.dart';
+
+class DetailScreen extends StatefulWidget {
   final title, thumb, id;
 
   const DetailScreen({
@@ -11,12 +15,28 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodeById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text(
-            title,
+            widget.title,
             style: const TextStyle(
               fontSize: 24,
             ),
@@ -34,7 +54,7 @@ class DetailScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Hero(
-                  tag: id,
+                  tag: widget.id,
                   child: Container(
                     width: 250,
                     clipBehavior: Clip.hardEdge,
@@ -47,11 +67,47 @@ class DetailScreen extends StatelessWidget {
                             color: Colors.black.withOpacity(0.3),
                           ),
                         ]),
-                    child: Image.network(thumb),
+                    child: Image.network(widget.thumb),
                   ),
                 ),
               ],
             ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return const RefreshProgressIndicator();
+              },
+            )
           ],
         ));
   }
