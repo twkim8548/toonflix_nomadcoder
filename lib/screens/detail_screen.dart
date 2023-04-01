713b6639e8a32bd4f75fab2c1adfece9c2c5251c
김태웅 <rlaxodnd95@gmail.com>
2023-04-01 16:05:52 +0900

    완성   - 좋아요/좋아요 취소 기능 추가   - Shared Preference 이용

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonflix/model/webtoon_episode_model.dart';
 import 'package:toonflix/services/api_service.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../model/webtoon_detail_model.dart';
+import '../widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final title, thumb, id;
@@ -21,6 +25,23 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id)) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      await prefs.setStringList('likedToons', []);
+    }
+  }
 
   @override
   void initState() {
@@ -28,6 +49,23 @@ class _DetailScreenState extends State<DetailScreen> {
 
     webtoon = ApiService.getToonById(widget.id);
     episodes = ApiService.getLatestEpisodeById(widget.id);
+
+    initPrefs();
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
   }
 
   @override
@@ -41,6 +79,14 @@ class _DetailScreenState extends State<DetailScreen> {
               fontSize: 24,
             ),
           ),
+          actions: [
+            IconButton(
+              onPressed: onHeartTap,
+              icon: Icon(isLiked
+                  ? Icons.favorite_outlined
+                  : Icons.favorite_outline_outlined),
+            ),
+          ],
           elevation: 2,
           backgroundColor: Colors.white,
           foregroundColor: Colors.green,
@@ -113,44 +159,9 @@ class _DetailScreenState extends State<DetailScreen> {
                         return Column(
                           children: [
                             for (var episode in snapshot.data!)
-                              Container(
-                                margin: const EdgeInsets.only(bottom: 10),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  border: Border.all(
-                                      color: Colors.green.shade400, width: 1),
-                                  borderRadius: BorderRadius.circular(20),
-                                  boxShadow:  [
-                                    BoxShadow(
-                                      blurRadius: 15,
-                                      offset: const Offset(10,10),
-                                      color: Colors.green.withOpacity(0.2),
-                                    )
-                                  ],
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 10,
-                                    horizontal: 20,
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        episode.title,
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.green.shade400,
-                                        ),
-                                      ),
-                                      Icon(
-                                        Icons.chevron_right_rounded,
-                                        color: Colors.green.shade400,
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                              Episode(
+                                episode: episode,
+                                webtoonId: widget.id,
                               )
                           ],
                         );
---
+++ lib/widgets/episode_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+import '../model/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    Key? key,
+    required this.episode,
+    required this.webtoonId,
+  }) : super(key: key);
+
+  final WebtoonEpisodeModel episode;
+  final String webtoonId;
+
+  onButtonTap() async {
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.green.shade400, width: 1),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 15,
+              offset: const Offset(10, 10),
+              color: Colors.green.withOpacity(0.2),
+            )
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.green.shade400,
+                ),
+              ),
+              Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.green.shade400,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
